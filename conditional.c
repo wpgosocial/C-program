452266fb55a0efa16ac0e04c95b7b5f4a6e3c235
wpgosocial <romeoclen3@gmail.com>
2023-04-12 23:27:03 -0400

    Conditional

--- conditional.c
+++ conditional.c
@@ -1,10 +1,24 @@
 #include <stdio.h>
 
-int main(void){
-    int x = get_int("What is X?: ");
-    int y = get_int("What is Y?: ");
 
-    if (x > y) {
-        printf("X is greater than Y\n");
-    }
+int main(void){
+    int x;
+    int y;
+     printf ("What is X?: ", &x);
+     scanf ("%dWhat is X?: ", &x);
+     printf ("What is y?: ", &y);
+     scanf ("%dWhat is Y?: ", &y);
+
+if (x > y){
+        printf("X is greater than Y\n" );
+}
+else if (x < y){
+
+    printf("Y is greater than x\n" );
+}
+else {
+    printf("X and Y are equal\n" );
+}
+
+        return 0;
 }
