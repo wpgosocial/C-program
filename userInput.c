ab6e882675bb128fcdf99d324a82c3b92252c8a0
wpgosocial <romeoclen3@gmail.com>
2023-04-23 07:37:39 -0400

    UserInput

---
+++ userInput.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    printf("Your number is: %d\n", num);
+
+    return 0;
+}
